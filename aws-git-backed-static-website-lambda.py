e32b6e45b220ba7d6e6df85916fae3d10241b1c7
Eric Hammond <ehammond@thinksome.com>
2016-11-12 14:10:32 -0800

    Clarify and switch to subprocess.

--- aws-git-backed-static-website-lambda.py
+++ aws-git-backed-static-website-lambda.py
@@ -17,45 +17,61 @@ import os
 import zipfile
 import tempfile
 import shutil
+import subprocess
 import traceback
 
 code_pipeline = boto3.client('codepipeline')
 
+def setup(event):
+    # Extract attributes passed in by CodePipeline
+    job_id = event['CodePipeline.job']['id']
+    job_data = event['CodePipeline.job']['data']
+    artifact = job_data['inputArtifacts'][0]
+    config = job_data['actionConfiguration']['configuration']
+    credentials = job_data['artifactCredentials']
+    from_bucket = artifact['location']['s3Location']['bucketName']
+    from_key = artifact['location']['s3Location']['objectKey']
+    from_revision = artifact['revision']
+    #output_artifact = job_data['outputArtifacts'][0]
+    #to_bucket = output_artifact['location']['s3Location']['bucketName']
+    #to_key = output_artifact['location']['s3Location']['objectKey']
+    user_parameters = config['UserParameters']
+
+    # Temporary credentials to access CodePipeline artifact in S3
+    key_id = credentials['accessKeyId']
+    key_secret = credentials['secretAccessKey']
+    session_token = credentials['sessionToken']
+    session = Session(aws_access_key_id=key_id,
+                      aws_secret_access_key=key_secret,
+                      aws_session_token=session_token)
+    s3 = session.client('s3',
+                        config=botocore.client.Config(signature_version='s3v4'))
+
+    return (job_id, s3, from_bucket, from_key, from_revision,
+            user_parameters)
+
+def download_source(s3, from_bucket, from_key, from_revision, source_dir):
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        #TBD: from_revision is not used here!
+        s3.download_file(from_bucket, from_key, tmp_file.name)
+        with zipfile.ZipFile(tmp_file.name, 'r') as zip:
+            zip.extractall(source_dir)
+
 def handler(event, context):
     try:
-        # Extract attributes passed in by CodePipeline
-        job_id = event['CodePipeline.job']['id']
-        job_data = event['CodePipeline.job']['data']
-        artifact = job_data['inputArtifacts'][0]
-        config = job_data['actionConfiguration']['configuration']
-        credentials = job_data['artifactCredentials']
-        from_bucket = artifact['location']['s3Location']['bucketName']
-        from_key = artifact['location']['s3Location']['objectKey']
-        from_revision = artifact['revision']
-        to_bucket = config['UserParameters']
+        (job_id, s3, from_bucket, from_key, from_revision,
+         user_parameters) = setup(event)
 
-        # Temporary credentials to access CodePipeline artifact in S3
-        key_id = credentials['accessKeyId']
-        key_secret = credentials['secretAccessKey']
-        session_token = credentials['sessionToken']
-        session = Session(aws_access_key_id=key_id,
-            aws_secret_access_key=key_secret,
-            aws_session_token=session_token)
-        s3 = session.client('s3',
-            config=botocore.client.Config(signature_version='s3v4'))
+        to_bucket = user_parameters
 
-        # Download from S3 the CodeCommit Git branch contents in ZIP file
-        tmpdir = tempfile.mkdtemp()
-        with tempfile.NamedTemporaryFile() as tmp_file:
-            s3.download_file(from_bucket, from_key, tmp_file.name)
-            # Unpack ZIP file
-            with zipfile.ZipFile(tmp_file.name, 'r') as zip:
-                zip.extractall(tmpdir)
+        # Directories for source content, and transformed static site
+        source_dir = tempfile.mkdtemp()
 
-        # Sync Git branch contents to S3 bucket
-        command = "./aws s3 sync --acl public-read --delete " + tmpdir + "/ s3://" + to_bucket + "/"
-        print(command)
-        print(os.popen(command).read())
+        # Download and unzip the source for the static site
+        download_source(s3, from_bucket, from_key, from_revision, source_dir)
+
+        # Generate static website from source
+        upload_static_site(source_dir, to_bucket)
 
         # Tell CodePipeline we succeeded
         code_pipeline.put_job_success_result(jobId=job_id)
@@ -67,6 +83,13 @@ def handler(event, context):
         code_pipeline.put_job_failure_result(jobId=job_id, failureDetails={'message': e, 'type': 'JobFailed'})
 
     finally:
-      shutil.rmtree(tmpdir)
+      shutil.rmtree(source_dir)
 
     return "complete"
+
+def upload_static_site(source_dir, to_bucket):
+    # Sync Git branch contents to S3 bucket
+    command = ["./aws", "s3", "sync", "--acl", "public-read", "--delete",
+               source_dir + "/", "s3://" + to_bucket + "/"]
+    print(command)
+    print(subprocess.check_output(command, stderr=subprocess.STDOUT))
